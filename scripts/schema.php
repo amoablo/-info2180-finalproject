2978c20ffda9c2d8b2cfdb166b4485f6b753c708
amoablo <ksterling542@gmail.com>
2020-12-02 01:55:11 -0500

    Still can't connect to mysql server

--- scripts/schema.php
+++ scripts/schema.php
@@ -5,22 +5,38 @@ $username = 'finalproject_user';
 $password = 'password123';
 $dbname = 'schema';
 
-    
-     //store information from form in variables
-     
+
+     //sanitize and escape information form input fields
      $fname = filter_var(addslashes($_REQUEST['fname']),  FILTER_SANITIZE_STRING);
      $lname = filter_var(addslashes($_REQUEST['lname']),  FILTER_SANITIZE_STRING);
      $password = filter_var($_REQUEST['pword'],  FILTER_SANITIZE_STRING);
      $email = filter_var(addslashes($_REQUEST['mail']), FILTER_SANITIZE_EMAIL);
  
+     $data = [$fname,$lname,$password,$email];
+
+      //check to ensure that no field is empty
+    foreach ($data as $field){
+        if(empty($field)){
+            exit("Could not connect to the database, field is empty:" );
+        }
+    }
+
+     //validate email
+     $regex = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix";
+    if (!preg_match($regex, $email)) {
+        exit("Could not connect to the database, email address is not valid.");
+    }
+
+    //check if numbers are used in the name fields
+    if(is_numeric($fname) || is_numeric($lname)){
+        exit("Could not connect to the database, invalid type use in name field.");
+    }
      
-         // There is at least one upper      // There is at least one number   // There is at least one regular letter  // Length of string is ay least 8
+    // There is at least one upper      // There is at least one number   // There is at least one regular letter  // Length of string is ay least 8
      if(preg_match('/[A-Z]/', $password) && preg_match('/[0-9]/', $password) && preg_match('/[a-z]/', $password) && strlen($password) >= 8 ){
          $hash = password_hash($password,PASSWORD_DEFAULT);
          
      }
-
-
 
     //connect to database
     try {
--- sql/GrantUser.sql
+++ sql/GrantUser.sql
@@ -1,2 +1,2 @@
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON schema.* TO 'finalproject_user'@'localhost' IDENTIFIED BY 'password123';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON schema TO 'finalproject_user'@'localhost' IDENTIFIED BY 'password123';
 
