479941a37a6a0e7861b1281bd3415ee77ecd5914
flxnaldo <iamnaldo7@gmail.com>
2020-12-29 18:45:01 -0500

    reverting for a final

---
+++ scripts/logout.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "./scriptsfunctions.php";
+session_start();
+
+if(isset($_SESSION['id'])){
+	session_unset();
+    session_destroy();
+    session_start();
+}
+
+redirect("../loginPrimaryPage.php");
+
+ ?>
